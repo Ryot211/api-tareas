edaf94d3e0892924f1b20d605d887add6d9f171f
Ryot211 <ryot211@gmail.com>
2025-06-13 17:26:19 -0500

    act routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,19 +1,8 @@
 const express = require("express");
-
 // vamos crear un objeto router para poder definir rutas
 const router  = express.Router();
-
 //importar el controlador que creamos en task.controller.js
-const {getAlltasks} = require("../controllers/task.controller");
-//metodo post
-const {createTask} = require("../controllers/task.controller");
-// metodo get por id
-const {getTaskByID} = require("../controllers/task.controller");
-// metodo delete por id 
-const {deleteTasksByID} = require("../controllers/task.controller");
-// metodo actualizar por id
-const {updateTaskId} = require("../controllers/task.controller");
-
+const {getAlltasks,createTask,getTaskByID,getAlltasks,updateTaskId} = require("../controllers/task.controller");
 router.get("/tasks",getAlltasks);
 //metodo post
 router.post("/tasks",createTask);
@@ -23,7 +12,6 @@ router.get("/tasks/:id_tasks",getTaskByID);
 router.delete("/tasks/:id_tasks",deleteTasksByID);
 // metodo actualizar por id 
 router.put("/tasks/:id_tasks",updateTaskId);
-
 //vamos a expportar eto para que se use en app.js
 
 module.exports = router;
